bd757c592ef87165422df82b28d188d631d41ca7
elizab123456 <160834667+elizab123456@users.noreply.github.com>
2024-06-26 13:44:19 +0300

    rrr

--- moving art work/sketch.js
+++ moving art work/sketch.js
@@ -1,7 +1,32 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const fallingObject = document.getElementById('falling-object');
-
-    fallingObject.addEventListener('mouseover', function() {
-        fallingObject.style.top = '500px';
-    });
-});
+// Load the image and create a p5.Image object.
+function preload() {
+    img = loadImage("./download.jpeg");
+  }
+  
+  function setup(){
+  
+  
+      createCanvas(windowWidth, windowHeight);
+  }
+  
+  function draw(){
+      image(img, 0, 0, width, height);
+      if (
+          mouseX <width / 2
+          &&
+          mouseY < height / 2){
+          circle(mouseX, mouseY, 50);
+      }
+      
+      if (
+          mouseX > width / 2
+          &&
+          mouseY > height / 2){
+           square(mouseX, mouseY, 50);}    
+          
+  
+     
+      
+      }
+      
+      
